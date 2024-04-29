54e71dd861e1759b2777bb4f40f5474a685ee913
Sylvia Chen <xschen@berkeley.edu>
2024-04-29 16:30:07 -0700

    added death timers, fixed colors, tweaked interaction probabilities, runs faster

--- src/cell_funcs.cpp
+++ src/cell_funcs.cpp
@@ -6,6 +6,10 @@
 #include "cell_funcs.h"
 #include "chunk.h"
 
+bool isAbovePercentage(float percentage) {
+	return (float)rand() / RAND_MAX > percentage;
+}
+
 void updateError(Chunk* chunk, vec3 pos) {
 	//std::cout << "UPDATING ELEMENT WHICH YOU SHOULD NOT " << std::endl;
 	return;
@@ -108,28 +112,40 @@ void updateFire(Chunk* chunk, vec3 curr_pos) {
 							 vec3(-1, 1, 1),
 							 vec3(-1, 1, 0),
 	};
+	// dissipate with a chance
+	if (isAbovePercentage(0.9)) {
+		chunk->setCell(curr_pos, cell{ AIR_COLOR, AIR });
+		return;
+	}
 
 	std::vector<vec3> burnable;
 	for (int i = 0; i < sizeof(dirs) / sizeof(vec3); i++) {
-		if (chunk->getCell(curr_pos + dirs[i]).type == WATER) {
-			// if any of the adjacent cells are water, burn out
+		if (chunk->getCell(curr_pos + dirs[i]).type == WATER) { 
+			// if any of the adjacent cells are water, burn out immediately
 			chunk->setCell(curr_pos + dirs[i], cell{ STEAM_COLOR, STEAM });
 			chunk->setCell(curr_pos, cell{ STEAM_COLOR, STEAM });
 			return;
 		}
-		else if (chunk->getCell(curr_pos + dirs[i]).type != AIR && chunk->getCell(curr_pos + dirs[i]).type != FIRE) {
+		else if (type2prop[chunk->getCell(curr_pos + dirs[i]).type]&PROPERTY_BURNABLE) {
 			burnable.push_back(dirs[i]);
 		}
 	}
 
-	// if there is anything to burn in adjacent cells, burn and spread
+	// if the block directly above is empty, chance to emit smoke
+	if (chunk->getCell(curr_pos + vec3(0, 1, 0)).type == AIR && isAbovePercentage(0.9)) {
+		chunk->setCell(curr_pos + vec3(0, 1, 0), cell{ SMOKE_COLOR, SMOKE });
+	}
+
+	// if there is anything to burn in adjacent cells, chance to burn and spread
 	// otherwise, chance to burn out
-	if (burnable.size() == 0 && (float) rand() / RAND_MAX > 0.9) {
-		chunk->setCell(curr_pos, cell{ color{ 0, 0, 0, 0 }, AIR });
+	if (burnable.size() == 0 && isAbovePercentage(0.9)) {
+		chunk->setCell(curr_pos, cell{ AIR_COLOR, AIR });
 	}
 	else {
 		for (int i = 0; i < burnable.size(); i++) {
-			chunk->setCell(curr_pos + burnable[i], cell{FIRE_COLOR, FIRE});
+			if (isAbovePercentage(0.7)) {
+				chunk->setCell(curr_pos + burnable[i], cell{ FIRE_COLOR, FIRE });
+			}
 		}
 		if (chunk->getCell(curr_pos + vec3(0, -1, 0)).type == AIR) {
 			chunk->swapCells(curr_pos, curr_pos + vec3(0, -1, 0));
@@ -147,7 +163,42 @@ void updateSteam(Chunk* chunk, vec3 curr_pos) {
 				   vec3(-1, 1, 1),
 				   vec3(-1, 1, 0),
 	};
-	if (chunk->getCell(curr_pos + vec3(0, 1, 0)).type == AIR && (float)rand() / RAND_MAX > 0.4) {
+	// dissipate with a chance
+	if (isAbovePercentage(0.7)) {
+		chunk->setCell(curr_pos, cell{ AIR_COLOR, AIR });
+		return;
+	}
+	if (chunk->getCell(curr_pos + vec3(0, 1, 0)).type == AIR && isAbovePercentage(0.4)) {
+		chunk->swapCells(curr_pos, curr_pos + vec3(0, 1, 0));
+	}
+	else {
+		int size = 8;
+		while (size != 0) {
+			vec3 new_pos = curr_pos + get_random(size, dirs);
+			if (chunk->getCell(new_pos).type == AIR) {
+				chunk->swapCells(curr_pos, new_pos);
+				return;
+			}
+		}
+	}
+}
+
+void updateSmoke(Chunk* chunk, vec3 curr_pos) {
+	vec3 dirs[] = { vec3(-1, 1, -1),
+				   vec3(0, 1, -1),
+				   vec3(1, 1, -1),
+				   vec3(1, 1, 0),
+				   vec3(1, 1, 1),
+				   vec3(0, 1, 1),
+				   vec3(-1, 1, 1),
+				   vec3(-1, 1, 0),
+	};
+	// dissipate with a chance
+	if (isAbovePercentage(0.7)) {
+		chunk->setCell(curr_pos, cell{ AIR_COLOR, AIR });
+		return;
+	}
+	if (chunk->getCell(curr_pos + vec3(0, 1, 0)).type == AIR && isAbovePercentage(0.4)) {
 		chunk->swapCells(curr_pos, curr_pos + vec3(0, 1, 0));
 	}
 	else {
--- src/cell_funcs.h
+++ src/cell_funcs.h
@@ -8,10 +8,13 @@ inline uint8_t getRandom(uint8_t from, uint8_t to) {
 	return rand()%(to-from) + from;
 };
 
+#define WALL_COLOR {0, 0, 0, 255}
+#define AIR_COLOR {0, 0, 0, 0}
 #define SAND_COLOR {getRandom(235, 255), getRandom(220, 255), getRandom(0, 10), 255}
 #define WATER_COLOR {0, 0, 255, 50}
 #define FIRE_COLOR {getRandom(235, 255), getRandom(0, 10), getRandom(0, 5), 255}
 #define STEAM_COLOR {getRandom(120, 136), getRandom(120, 136), getRandom(120, 136), 50}
+#define SMOKE_COLOR {getRandom(60, 80), getRandom(60, 80), getRandom(60, 80), 255}
 
 #define GET_COLOR_FUNC(CLR) []() {return color CLR; }
 
@@ -23,5 +26,6 @@ extern void updateSand(Chunk* chunk, vec3 pos);
 extern void updateWater(Chunk* chunk, vec3 pos);
 extern void updateFire(Chunk* cunk, vec3 pos);
 extern void updateSteam(Chunk* cunk, vec3 pos);
+extern void updateSmoke(Chunk* cunk, vec3 pos);
 
 #endif
